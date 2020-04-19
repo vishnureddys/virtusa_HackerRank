e255a17c571d9170ccc281650731b6d52145267e
Vishnu S Reddy <52468309+vishnureddys@users.noreply.github.com>
2020-04-19 23:51:55 +0530

    Create Two_Strings.py
    All test cases pass withing the given time frame.

---
+++ Two_Strings.py
@@ -0,0 +1,37 @@
+###
+The link to the problem statement.
+https://hackerrank-challenge-pdfs.s3.amazonaws.com/5187-two-strings-English?AWSAccessKeyId=AKIAJ4WZFDFQTZRGO3QA&Expires=1587324042&Signature=d4O0krpcemkAZQJMu%2BlSyoB272g%3D&response-content-disposition=inline%3B%20filename%3Dtwo-strings-English.pdf&response-content-type=application%2Fpdf
+###
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the twoStrings function below.
+def twoStrings(s1, s2):
+    numOfOccurances = [0] * 26
+    for i in s1:
+        numOfOccurances[ord(i)-ord('a')] = True
+    for i in s2:
+        if(numOfOccurances[ord(i)-ord('a')] == True):
+            return "YES"
+    return "NO"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s1 = input()
+
+        s2 = input()
+
+        result = twoStrings(s1, s2)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
